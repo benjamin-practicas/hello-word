08ae33072bbde752dbd1445f21704ef55ddb649c
benjamin-practicas <69986645+benjamin-practicas@users.noreply.github.com>
2021-03-09 11:28:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>***TEXTO DESDE HTML***</h1>
+</body>
+</html>
+ 
 <?php
 
 
-$variable="****PRUEBA****"
+$variable="****PRUEBA****";
 
- echo variable;
+ echo $variable;
 
 
 
